47be4b0fe634413ac26d34c83c4d3371e057d3e3
Yufeng Wang <wwangyyff@hotmail.com>
2018-08-29 15:11:12 -0700

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -19,6 +19,8 @@ namespace DatingApp.API.Controllers
         {
             _context = context;
         }
+        
+        [AllowAnonymous]
         // GET api/values
         [HttpGet]
         public async Task<IActionResult> GetValues()
